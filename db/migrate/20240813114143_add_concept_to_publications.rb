a9154d30840f4bb5da65beae763240c9ac5b6724
Jochen Klar <mail@jochenklar.de>
2024-08-24 09:50:27 +0200

    Update migration to create concepts (and concept dois) for all existing publications

--- db/migrate/20240813114143_add_concept_to_publications.rb
+++ db/migrate/20240813114143_add_concept_to_publications.rb
@@ -1,5 +1,14 @@
 class AddConceptToPublications < ActiveRecord::Migration[5.2]
   def change
     add_column :publications, :concept_id, :integer
+
+    Publication.all.each do |publication|
+      doi = publication.doi
+      unless doi.nil?
+        concept = Concept.create_for_doi!(doi)
+        publication.concept = concept
+        publication.save!
+      end
+    end
   end
 end
