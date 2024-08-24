f88e5f4c5951e111b39dc7bcb847fefa6abfd753
Jochen Klar <mail@jochenklar.de>
2024-08-24 10:35:33 +0200

    Fix concept migration

--- db/migrate/20240813114143_add_concept_to_publications.rb
+++ db/migrate/20240813114143_add_concept_to_publications.rb
@@ -2,10 +2,31 @@ class AddConceptToPublications < ActiveRecord::Migration[5.2]
   def change
     add_column :publications, :concept_id, :integer
 
-    Publication.all.each do |publication|
+    Publication.order(:created_at).each do |publication|
       doi = publication.doi
-      unless doi.nil?
-        concept = Concept.create_for_doi!(doi)
+      element = publication.element
+
+      unless doi.nil? or element.nil?
+        if publication.element_type == 'Container'
+          previous_version = element.extended_metadata['previous_version_id']
+          if previous_version.nil?
+            concept = Concept.create_for_doi!(doi)
+          else
+            previous_publication = Publication.find_by(element_type: 'Container', element_id: previous_version)
+            concept = previous_publication.concept
+            concept.update_for_doi!(doi)
+          end
+        else
+          previous_version = element.tag.taggable_data.dig('previous_version', 'id')
+          if previous_version.nil?
+            concept = Concept.create_for_doi!(doi)
+          else
+            previous_publication = Publication.find_by(element_type: publication.element_type, element_id: previous_version)
+            concept = previous_publication.concept
+            concept.update_for_doi!(doi)
+          end
+        end
+
         publication.concept = concept
         publication.save!
       end
